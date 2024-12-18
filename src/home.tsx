ff59408568e4a95fa6afe9fb47e65044c60b1aff
tuoasty <tuoasty@gmail.com>
2024-12-18 13:59:08 +0700

    first commit
    first commit

---
+++ src/home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar.tsx";
+
+function HomePage(){
+  return (
+    <div className="h-screen w-screen">
+      <Navbar />
+      <div className="pt-[5rem]">
+          <p>Test</p>
+
+      </div>
+    </div>
+  ) 
+}
+
+export default HomePage;
