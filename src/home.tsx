e74f325c1a34c80f60dd140c5cd33193b068582a
tuoasty <tuoasty@gmail.com>
2024-12-19 12:13:32 +0700

    feat navbar arrow

--- src/home.tsx
+++ src/home.tsx
@@ -1,11 +1,10 @@
-import Navbar from "./components/navbar.tsx";
+import Navbar from "./components/navbar/navbar.tsx";
 
 function HomePage(){
   return (
     <div className="h-screen w-screen">
       <Navbar />
       <div className="pt-[5rem]">
-          <p>Test</p>
 
       </div>
     </div>
