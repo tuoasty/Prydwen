a67505c9c48970507be2342f3d482b1c3ce29abb
tuoasty <tuoasty@gmail.com>
2024-12-21 13:02:31 +0700

    feat sidebutton

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App(){
     <div className="h-screen w-screen flex">
       <SidebarProvider>
         <Sidebar/>
-        <div className="w-[0.6px] h-screen bg-zinc-700"></div>
+        <div className="w-[1px] h-screen bg-zinc-700"></div>
         <div className="flex-1 relative">
           <Navbar/>
-          <div className="pt-[4.5rem] bg-zinc-900 h-full w-full">
-            <div className="w-full h-[0.6px] bg-zinc-700"></div>
+          <div className="pt-[4.5rem] bg-zinc-800 h-full w-full">
+            <div className="w-full h-[1px] bg-zinc-700"></div>
             {/*  Content*/}
           </div>
         </div>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,16 +1,22 @@
 import logo from "./assets/logo.png"
 import minlogo from "./assets/minlogo.webp"
 import {useSidebar} from "../../context/useSidebar.ts";
+import { TiHome } from "react-icons/ti";
+
 
 export default function Sidebar(){
     const { isOpen } = useSidebar();
 
     return(
-      <aside className={`h-full bg-zinc-900 flex-col ${isOpen ? "w-[14%]" : "w-[4%]"}`}>
+      <aside className={`h-full bg-zinc-900 flex-col ${isOpen ? "w-[17%]" : "w-[5%]"}`}>
         <div className="h-[4.5rem] flex place-content-center">
           <img src={isOpen ? logo : minlogo} alt="logo" className="h-10 m-auto"/>
         </div>
         <div className="w-full h-[0.6px] bg-zinc-700"></div>
+        <button className="w-full h-14 bg-white flex items-center p-2">
+          <TiHome className="h-10 ml-6 size-5"/>
+          <h2 className="text-center ml-3">Home</h2>
+        </button>
       </aside>
     )
 }
