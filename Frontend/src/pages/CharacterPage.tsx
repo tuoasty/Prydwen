ed7c89e7bbd301c6aed598254b28eaa92f7a11b2
tuoasty <tuoasty@gmail.com>
2025-01-09 12:13:27 +0700

    feat: page component

--- Frontend/src/pages/CharacterPage.tsx
+++ Frontend/src/pages/CharacterPage.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { fetchCharacters } from '../services/api.ts';
 import { Character } from '../../../shared/types/character.ts';
-import { bgColor } from '../components/details/color.ts';
+import icon from '../assets/images/character/icon.webp';
+import Page from '../components/page/Page.tsx';
+import PageHeader from '../components/page/pageheader/PageHeader.tsx';
 
 export default function CharacterPage(){
   const [characters, setCharacters] = useState([]);
@@ -11,15 +13,17 @@ export default function CharacterPage(){
   }, []);
 
   return (
-    <div className="overflow-x-auto h-full">
-      {characters.map((character:Character) => (
-        <div className={`${bgColor[character.element.color as keyof typeof bgColor]}`} key={character.id}>
+    <Page>
+      <PageHeader page="Characters List" title="Honkai: Star Rail Characters List" description="List of all available characters in Honkai: Star Rail" date="08/01/2025"
+      image={icon}/>
+      {characters.map((character: Character) => (
+        <div className={``} key={character.id}>
           <h2>{character.name}</h2>
           <h2>{character.path.name}</h2>
           <img src={character.element.imageUrl} />
           <img src={character.imageUrl} />
         </div>
       ))}
-    </div>
+    </Page>
   )
 }
