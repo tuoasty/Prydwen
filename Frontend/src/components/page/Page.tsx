87f891a7a3b5a4f3abce1a409a33d87aea760cbc
tuoasty <tuoasty@gmail.com>
2025-01-13 14:42:16 +0700

    feat: dynamic grid

--- Frontend/src/components/page/Page.tsx
+++ Frontend/src/components/page/Page.tsx
@@ -9,7 +9,7 @@ export default function Page(p:Props){
     <div className="relative overflow-x-auto h-full bg-zinc-800 -z-20">
       {/* Background */}
       <div className="absolute w-full h-full bg-zinc-800 -z-10 overflow-hidden">
-        <img src={starrail} className="opacity-35 scale-150 translate-y-20" />
+        <img src={starrail} className="opacity-35 scale-150 translate-y-20 w-full" />
         <div className="absolute inset-0 h-full bg-color- bg-gradient-to-b from-zinc-800/80 to-zinc-800"></div>
       </div>
 
--- Frontend/src/pages/CharacterPage.tsx
+++ Frontend/src/pages/CharacterPage.tsx
@@ -1,29 +1,62 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { fetchCharacters } from '../services/api.ts';
 import { Character } from '../../../shared/types/character.ts';
 import icon from '../assets/images/character/icon.webp';
 import Page from '../components/page/Page.tsx';
 import PageHeader from '../components/page/pageheader/PageHeader.tsx';
+import { useSidebar } from '../context/useSidebar.ts';
 
 export default function CharacterPage(){
   const [characters, setCharacters] = useState([]);
+  const [columns, setColumns] = useState('grid-cols-7');
+  const containerRef = useRef<HTMLDivElement>(null);
+  const {isOpen} = useSidebar();
+
+  const calculateColumns = (width: number) => {
+    if(width > 2100) return 'grid-cols-12';
+    else if(width > 1920) return 'grid-cols-11';
+    else if(width > 1700) return 'grid-cols-10';
+    else if(width > 1440) return 'grid-cols-9';
+    else if(width > 1280) return 'grid-cols-8';
+    else if(width > 1200) return 'grid-cols-7';
+    else if(width > 950) return 'grid-cols-6';
+    else if(width > 800) return 'grid-cols-5';
+    else if(width > 735) return 'grid-cols-4';
+    else return 'grid-cols-3';
+  }
+
+  const updateColumns = () => {
+    const containerWidth:number = containerRef.current?.offsetWidth || 0;
+    setColumns(calculateColumns(containerWidth));
+  }
 
   useEffect(() => {
     fetchCharacters().then(setCharacters).catch(console.error);
-  }, []);
+    updateColumns();
+
+    window.addEventListener('resize', updateColumns);
+
+    return () => {
+      window.removeEventListener('resize', updateColumns);
+    }
+  }, [isOpen]);
 
   return (
     <Page>
       <PageHeader page="Characters List" title="Honkai: Star Rail Characters List" description="List of all available characters in Honkai: Star Rail" date="08/01/2025"
       image={icon}/>
-      {characters.map((character: Character) => (
-        <div className={``} key={character.id}>
-          <h2>{character.name}</h2>
-          <h2>{character.path.name}</h2>
-          <img src={character.element.imageUrl} />
-          <img src={character.imageUrl} />
-        </div>
-      ))}
+      {/*${bgColor[character.element.color as keyof typeof bgColor]}*/}
+      <div ref={containerRef} className={`grid gap-4 p-20 ${columns}`}>
+        {characters.map((character: Character) => (
+          <div className="w-36 h-48 object-cover overflow-hidden bg-blue-500" key={character.id}>
+            <h2>{character.name}</h2>
+            <h2>{character.path.name}</h2>
+            <img src={character.element.imageUrl} />
+            <img src={character.imageUrl} />
+          </div>
+        ))}
+      </div>
+
     </Page>
   )
 }
